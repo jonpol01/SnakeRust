6b0afd8c7d944e7010ce4154e9d2a8faab0b5beb
John Paul Soliva <soliva.johnpaul@icloud.com>
2023-05-19 01:56:41 +0900

    bug/fix: head not moving, or entering the iterations
    Signed-off-by: John Paul Soliva <soliva.johnpaul@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use bevy::prelude::*;
+use bevy::{prelude::*};
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+#[derive(Component)]
+struct SnakeHead {
+    direction: Direction,
+}
 
 fn setup_camera(mut commands: Commands) {
     // commands spawn bundle orthographic camera bundle new 2d
     commands.spawn(Camera2dBundle::default());
-
-
-//    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
 fn spawn_snake(mut commands: Commands) {
@@ -33,20 +35,60 @@ fn spawn_snake(mut commands: Commands) {
 }
 
 fn snake_movement(mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
-    for (_head, mut transform) in head_positions.iter_mut() {
-        transform.translation.x += 2.;
+
+    // move the snake head
+//    for (_head, transform) in &mut head_positions{
+    for (_snakehead, transform) in &mut head_positions{
+        info!("Head: {:?}", transform.translation);
+        //let forward = transform.forward();
+        //transform.translation += forward + 2.0;
+        //transform.translation.x += 2.;
     }
+
+    // for (_head, mut transform) in &mut head_positions {
+    //     transform.translation.y += 2.;
+    // }
 }
 
-#[derive(Component)]
-struct SnakeHead;
+fn snake_move(
+    keyboard_input: Res<Input<KeyCode>>, 
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+){
+    for mut transform in &mut head_positions {
+        if keyboard_input.pressed(KeyCode::Left){
+            info!("Left");
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right){
+            info!("Right");
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down){
+            info!("Down");
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up){
+            info!("Up");
+            transform.translation.y += 2.;
+        }
+    }
+
+}
+
+// This system will move the cube forward.
+// fn move_cube(mut cubes: Query<(&mut Transform, &mut CubeState)>, timer: Res<Time>) {
+//     for (mut transform, cube) in &mut cubes {
+//         // Move the cube forward smoothly at a given move_speed.
+//         let forward = transform.forward();
+//         transform.translation += forward * cube.move_speed * timer.delta_seconds();
+//     }
+// }
 
 fn main() {
     App::new()
+    .add_plugins(DefaultPlugins)
     .add_startup_system(setup_camera)
     .add_startup_system(spawn_snake)
-
-    .add_system(snake_movement) // <--
-    .add_plugins(DefaultPlugins)
+    .add_system(snake_movement)
     .run();
 }
